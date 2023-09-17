76296b41429fbf6f6f549f627f10708326cc4fac
manuelluevano <manuelluevano@hotmail.com>
2023-09-17 15:49:00 -0600

    test upaload img 5

--- routes/refacciones.js
+++ routes/refacciones.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const refaccionController = require("../controllers/refacciones");
 const multer = require("multer");
+const path = require('path')
 
 //importar middleware
 const check = require("../middlewares/auth");
@@ -9,19 +10,26 @@ const check = require("../middlewares/auth");
 //CONFIGURACION DE SUBIDA DE ARCHIVO
 const almacenamiento = multer.diskStorage({
   destination: (req, file, cb) => {
-            //Indicar donde es el destino de subida de archivo
-    cb(null, "./uploads");
+    //Indicar donde es el destino de subida de archivo
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    cb(null, "image" + Date.now() + file.originalname)
-},
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 const upload = multer({ storage: almacenamiento });
 
-
 router.get("/prueba-refaccion", refaccionController.pruebaRefaccion);
-router.post("/refaccion", [upload.single("file0")], check.auth, refaccionController.addRefaccion);
+router.post(
+  "/refaccion",
+  [upload.single("file0")],
+  check.auth,
+  refaccionController.addRefaccion
+);
 router.get("/refacciones", check.auth, refaccionController.listRefaccion);
 //Exportar router
 module.exports = router;
